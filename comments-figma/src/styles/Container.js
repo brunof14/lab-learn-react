2d1879fe58615918d2d394484716b5bed7231524
Bruno Felipe <oliveiraf.120@gmail.com>
2022-03-02 15:26:08 -0400

    refactor: move struct of comment to a new component Comment, and change file name of styled component Container

--- comments-figma/src/styles/Container.js
+++ comments-figma/src/styles/Container.js
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 export const Container = styled.div`
   max-width: 72rem;
   margin: 0 auto;
   padding: 1.2rem;
-`
+`;
